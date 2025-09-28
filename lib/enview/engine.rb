3d05d0f401a62ec0bb1bfee48b4f868825e1fad6
enrico <enrico.gallus@gmail.com>
2025-09-28 10:19:49 +0900

    fix path

--- lib/enview/engine.rb
+++ lib/enview/engine.rb
@@ -5,8 +5,5 @@ module Enview
     config.app_generators do |g|
       g.template_engine :enview
     end
-
-    config.autoload_paths   << root.join("app", "components")
-    config.eager_load_paths << root.join("app", "components")
   end
 end
