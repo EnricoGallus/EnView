a953acf153876052fd5f5dc2f32f18790ec98e3c
enrico <enrico.gallus@gmail.com>
2025-10-11 08:46:56 +0900

    use correct structure

--- lib/generators/view_component/enview/enview_generator.rb
+++ lib/generators/view_component/enview/enview_generator.rb
@@ -2,8 +2,10 @@
 
 require "generators/view_component/erb/erb_generator"
 
-module Enview
+module ViewComponent
   module Generators
+    # Template engine adapter so that `rails g view_component NAME` uses Enview templates
+    # when `config.app_generators.template_engine = :enview` is set by the engine.
     class EnviewGenerator < ViewComponent::Generators::ErbGenerator
       source_root File.expand_path("templates", __dir__)
     end
