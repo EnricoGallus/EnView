da61bba0b0ec0384642e2ae89ba346c71149b9e6
enrico <enrico.gallus@gmail.com>
2025-10-11 08:04:37 +0900

    override rails scaffold generator

--- lib/enview/engine.rb
+++ lib/enview/engine.rb
@@ -1,4 +1,7 @@
 module Enview
   class Engine < ::Rails::Engine
+    # Keep engine lean: do not force a global template_engine.
+    # This allows host apps to use their own generators (e.g. view_component)
+    # and only override what we explicitly provide.
   end
 end
